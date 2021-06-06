835ccbb6f07ba60a7cfaa398b7a36ce9eb5b10ac
kiwibrie <brianne0128@gmail.com>
2021-06-05 21:24:58 -0500

    working base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -10,6 +10,32 @@ public class App {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args){
-        
+        App myApp = new App();
+
+        int euros = myApp.promptEuros();
+        double rate = myApp.promptRate();
+
+        double dollars = myApp.calculateDollars(euros, rate);
+
+        myApp.printOutput(euros, rate, dollars);
+    }
+
+    public void printOutput(int euros, double rate, double dollars){
+        System.out.printf("%d Euros at an exchange rate of %f is\n" +
+                "%.2f U.S. dollars.", euros, rate, dollars);
+    }
+
+    public double calculateDollars(int euros, double rate){
+        return euros * rate;
+    }
+
+    public int promptEuros(){
+        System.out.println("How many euros are you exchanging? ");
+        return in.nextInt();
+    }
+
+    public double promptRate(){
+        System.out.println("What is the exchange rate? ");
+        return in.nextDouble();
     }
 }
